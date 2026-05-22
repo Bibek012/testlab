2aabac045c4ee710eb5599d55486bb2f71ba2418
Bibek Shaw <bibekshaw9874@gmail.com>
2026-05-22 01:37:00 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/exams/[category]/[examId]/page.tsx
+++ src/app/exams/[category]/[examId]/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useMemo } from "react";
@@ -138,7 +137,7 @@ export default function ExamDashboardPage() {
             </section>
 
             <Leaderboard />
-          </div>
+          </aside>
         </div>
       </div>
 
